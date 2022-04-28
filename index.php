75dc620ff25298d3458dc77009850b28c796bcf4
CedricBaude <baude.cedric.perso@gmail.com>
2022-04-28 16:43:23 +0200

    formulaire v1

--- index.php
+++ index.php
@@ -12,12 +12,15 @@
 
     <div class="container">
         <div class="row">
-            <nav class="col-md-3 ">
+            <nav class="col-md-3 mt-3 ">
                 <a href="index.php" class="btn btn-outline-secondary w-100" role="button">Home</a>
             </nav>
 
-            <section class="col-9">
-                <a href="index.php" class="btn btn-primary" role="button">Ajouter des données</a>
+            <section class="col-9 mt-3">
+                <a href="index.php?add" class="btn btn-primary" role="button">Ajouter des données</a>
+
+                <?php  include 'includes/form.inc.html'; ?>
+                
 
             </section>
         </div>
@@ -27,7 +30,7 @@
 
 
 
-    <?php include_once './includes/footer.inc.html' ; ?>
+    <br><?php include_once './includes/footer.inc.html' ; ?>
 </body>
 </html>
 
