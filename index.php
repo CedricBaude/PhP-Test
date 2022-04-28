d92da49b5ad5e75ab7ffd115716366f7b71ccb6d
CedricBaude <baude.cedric.perso@gmail.com>
2022-04-28 11:08:43 +0200

    Mise en place structure html

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php
+<?php  ?>
 
-phpinfo();
+
+<!DOCTYPE html>
+<html lang="fr">
+    <?php include_once './includes/head.inc.html' ; ?>
+<body>
+    <?php include_once './includes/header.inc.html' ; ?>
+
+    <div class="container">
+        
+    </div>
+
+</body>
+</html>
+
