af124dd518aac9e8bd57dda8ce9c0a2d8b639735
CedricBaude <baude.cedric.perso@gmail.com>
2022-04-28 11:31:55 +0200

    Mise en place structure html

--- index.php
+++ index.php
@@ -3,11 +3,19 @@
 
 <!DOCTYPE html>
 <html lang="fr">
-    <?php include_once './includes/head.inc.html' ; ?>
+    
+    <?php include_once './includes/head.inc.html' ;  ?>
+
 <body>
+
     <?php include_once './includes/header.inc.html' ; ?>
 
     <div class="container">
+        <div class="row">
+            <nav class="col-md-3 mt-3">
+                <a href="index.php" class="btn btn-outline-secondary w-100" role="button">Home</a>
+            </nav>
+        </div>
 
     </div>
 
