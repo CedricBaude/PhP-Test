e04ff956222db07157f77ebf7d2af40461a4e140
CedricBaude <baude.cedric.perso@gmail.com>
2022-04-29 17:12:43 +0200

    maj liste

---
+++ includes/ul.inc.php
@@ -0,0 +1,8 @@
+
+<ul class="nav flex-column my-0">
+    <li class="nav-item"><a class="btn btn-outline-secondary w-100" href="index.php?debugging">Débogage</a></li>
+    <li class="nav-item"><a class="btn btn-outline-secondary w-100" href="index.php?concatenation">Concaténation</a></li>
+    <li class="nav-item"><a class="btn btn-outline-secondary w-100" href="index.php?loop">Boucle</a></li>
+    <li class="nav-item"><a class="btn btn-outline-secondary w-100" href="index.php?function">Fonction</a></li>
+    <li class="nav-item"><a class="btn btn-outline-secondary w-100" href="index.php?del">Supprimer</a></li>
+</ul>
--- index.php
+++ index.php
@@ -16,15 +16,75 @@ if(isset($_SESSION['table'])) $table = $_SESSION['table'];
         <div class="row">
             <nav class="col-md-3 mt-3 ">
                 <a href="index.php" class="btn btn-outline-secondary w-100" role="button">Home</a>
+                <?php if (isset($table)) include_once './includes/ul.inc.php'; ?> 
             </nav>
 
             <section class="col-9 mt-3">
-                <a href="index.php?add" class="btn btn-primary" role="button">Ajouter des données</a>
+                <?php if(isset($_GET['add'])) {
+                        include './includes/form.inc.html';
+                    }
+                    elseif(isset($_POST['enregistrer'])) {
+                        $prenom = $_POST['user-prenom'];
+                        $nom = $_POST['user-nom'];
+                        $age = $_POST['user-age'];
+                        $taille = $_POST['user-taille'];
+                        $sex = $_POST['user-sex'];
+                        $table = array(          
+                            "first_name" => $prenom,
+                            "last_name"  =>  $nom,
+                            "age" => $age,
+                            "size" => $taille,
+                            "civility" => $sex,
+                        );
 
-                <?php  include 'includes/form.inc.html'; ?>
+                        $_SESSION["table"] = $table; 
+                        echo '<p class="alert-success text-center py-3"> Données sauvegardées !</p>';
+                    
+                    } elseif (isset($_GET["debugging"])) {
+                        echo '<h2>Débogage</h2>';
+                        echo "<p>===> Lecture du tableau à l'aide de la fonction print_r()</p>";
+                        print "<pre>";
+                        print_r($table);
+                        print "</pre>";
+                    
+                    
+                    } elseif (isset($_GET['del'])) {
+                        unset ($_SESSION['table']);
+                        if (empty($_SESSION['table'])) {
+                            echo '<p class="alert-success text-center py-3"> Données suprimées</p>';
+                        }
+                    }
+                    
+                    echo '<a role="button" class=" btn btn-primary" href="index.php?add">Ajouter des données</a>';
+                    
                 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                
+                ?>
+            
+
             </section>
+
         </div>
 
     </div>
